f421417af5cba5030a5215779b4050d8e3529a25
Philippe MICHEL <philippe@docteur-michel.fr>
2024-09-12 11:27:43 +0200

    Rapport (V1) OK

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ACR_90
-Étude observationnelle descriptive monocentrique rétrospective 
+![USRC](novo_usrc.png)
+
+<!-- badges: start -->  
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+<!-- badges: end -->
+
+# ACR 90
+
+Étude observationnelle descriptive monocentrique rétrospective sur une durée
+estimée de 4 mois.
+
+# Fichiers
+
+- `acr90_plan_analyse.qmd` : Plan d'analyse (un peu vieillot)
+- `rapport.md` : Rapport statistique
+- `import.R` : Macro d'import & de mise en forme des données
+- `_quarto.yml` : En-tête (Yaml & $\LaTeX$) pour les documents
+
+Le fichier $\LaTeX$ `generique.tex`contient des données personnelles & n'est donc pas disponible sur ce serveur. 
+
--- import.R
+++ import.R
@@ -21,19 +21,16 @@ importph <- function() {
   nan <- c("NA", "ND", "", " ", "non demandé")
 
   tt <- read_ods("datas/ACR90.ods", sheet = 1, na = nan)
-  bn <- read_ods("datas/ACR90.ods", sheet = 2)
-  var_label(tt) <- bn$bnom
   #
   tt <-  tt |>
     janitor::clean_names() |>
     remove_constant() |>
-    dplyr::mutate_if(is.character, as.factor) |>
-    dplyr::select(!starts_with("temoin")) |>
+    mutate(across(where(is.character) & !starts_with("temoin"), as.factor)) |>
     mutate(delai_mce = as.numeric(as.character(delai_mce))) |>
-    mutate(horaire = as.numeric(str_split(horaire, "h", simplify = TRUE)[, 1])) |>
-    mutate(horaire = as.factor(
+    mutate(periode = as.numeric(str_split(horaire, "h", simplify = TRUE)[, 1])) |>
+    mutate(periode = as.factor(
       cut(
-        horaire,
+        periode,
         include.lowest = TRUE,
         right = FALSE,
         dig.lab = 4,
@@ -44,11 +41,13 @@ importph <- function() {
     mutate(experience = fct_relevel(experience,
                                     "1-4 ans", "5-9 ans", ">10 ans"))
 #
-  var_label(tt$delai_mce) <- "Délais Initiation MCE"
+  bn <- c("id", "Délai decrochage/ reconnaissance ACR", "Motif de l'appel", "En faveur d'un ACR", "En défaveur d'un ACR", "Délais d'initiation du MCE",  "Ton de l'appel", "Sexe de l'ARM", "ancienneté de l'ARM", "Horaire", "Période")
+  var_label(tt) <- bn
 
   save(tt, bn, file = "datas/acr90.RData")
 }
 
+importph()
 load(file = "datas/acr90.RData")
 
 
